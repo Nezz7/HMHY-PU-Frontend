c8ff610f7f7d86d8f536ba6c0022563d2311970f
NEZZ <omarkamoun97@gmail.com>
2019-04-28 21:20:00 +0100

    Creating compoments

---
+++ src/app/all-messages/all-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-messages',
+  templateUrl: './all-messages.component.html',
+  styleUrls: ['./all-messages.component.css']
+})
+export class AllMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { TemplateComponent } from './template/template.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HelpmePostComponent } from './helpme-post/helpme-post.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AllMessagesComponent } from './all-messages/all-messages.component';
+import { ProfileSettingComponent } from './profile-setting/profile-setting.component';
+import { FormRequestComponent } from './form-request/form-request.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateComponent,
     NavBarComponent,
-    HelpmePostComponent
+    HelpmePostComponent,
+    ProfileComponent,
+    AllMessagesComponent,
+    ProfileSettingComponent,
+    FormRequestComponent
   ],
   imports: [
     BrowserModule,
